23abaeece5404db45f162724ea1f0114b25dd415
szl_work <sunzhilin3@qq.com>
2019-04-17 19:34:11 +0800

    27

---
+++ src/main/java/cn/leetcode/RemoveElement.java
@@ -0,0 +1,32 @@
+package cn.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+ *
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ *
+ */
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,3,5,3};
+        int val =3;
+        int length = new RemoveElement().removeElement(nums, val);
+        System.out.println(length);
+    }
+
+    public int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (int i : nums) {
+            if(i != val){
+                nums[index++] = i;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return index;
+    }
+}
